2b0f9ed4f68f2935528181416febcd5cdef1fcc1
Mykola1601 <nik160186@gmail.com>
2023-09-11 22:30:32 +0300

    1109232230

---
+++ clean_folder/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clean_folder',
+      version='1',
+      description='useful code',
+      url='https://github.com/Mykola1601/HW7/tree/main/clean_folder',
+      author='MIKOLA',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['clean_folder'])

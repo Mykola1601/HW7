9fb22b9631bfa2fbaad3b7eb39446d7e13568910
Mykola1601 <nik160186@gmail.com>
2023-09-20 22:51:15 +0300

    3

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name='clean_folder',
       version='0.1.15',
-      # description='useful code',
-      # url='https://github.com/Mykola1601/HW7/tree/main/clean_folder',
-      # author='MIKOLA',
+      description='useful code',
+      url='https://github.com/Mykola1601/HW7/tree/main/clean_folder',
+      author='MIKOLA',
       # author_email='fly@example.com',
-      # license='MIT',
-      # packages=['clean_folder']
+      license='MIT',
+      packages=['clean_folder']
       entry_points = {'console_scripts':['go = clean_folder.sort:sort' ]  }
       )
